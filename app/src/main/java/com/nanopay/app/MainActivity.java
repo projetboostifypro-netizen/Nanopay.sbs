ae5a979c1ea6b52c6f3d2bf95cc68fe63de7bf68
projetboostifypro-netizen <projetboostifypro@gmail.com>
2026-03-05 15:24:33 +0100

    Initial commit Nanopay

---
+++ app/src/main/java/com/nanopay/app/MainActivity.java
@@ -0,0 +1,19 @@
+package com.nanopay.app;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://nanopay.sbs");
+    }
+}
